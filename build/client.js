60af1d65f6699b45120809d4bf4dbf88e437d71a
jgus52 <srriaght@gmail.com>
2021-08-12 11:37:25 +0900

    edit build gitignore

--- build/client.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var PrismaClient = require("@prisma/client").PrismaClient;
-var client = new PrismaClient();
-exports.default = client;
